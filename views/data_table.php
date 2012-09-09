3bed4327a872b82678828be9ebffeb0ec3ca8988
Japh <japhie@gmail.com>
2012-09-09 23:09:21 +1000

    Cleaned up view files to conform with WordPress' and my own coding standards

--- views/data_table.php
+++ views/data_table.php
@@ -1,31 +1,31 @@
 <?php
-$heading_cells = "";
+$heading_cells = '';
 
-foreach ($headings as $heading)
-    $heading_cells .= "<th>{$heading}</th>";
-    
-$table_contents = "
-<thead>
-    <tr>
-        {$heading_cells}
-    </tr>
-</thead>
-<tfoot>
-    <tr>
-        {$heading_cells}
-    </tr>
-</tfoot>
-<tbody>";
-
-foreach($rows as $row) {
-    $row_contents = "";
-    foreach ($row as $cell) {
-        $row_contents .= $this->element('td', $cell);
-    }
-    $table_contents .= $this->element('tr', $row_contents);
+foreach ($headings as $heading) {
+	$heading_cells .= '<th>' . $heading . '</th>';
 }
 
-$table_contents .= "</tbody>";
+$table_contents = '
+<thead>
+	<tr>
+		' . $heading_cells . '
+	</tr>
+</thead>
+<tfoot>
+	<tr>
+		' . $heading_cells . '
+	</tr>
+</tfoot>
+<tbody>';
 
-echo $this->element('table', $table_contents, array_merge(array('class' => 'widefat'), $table_attributes));
-?>
+foreach ( $rows as $row ) {
+	$row_contents = '';
+	foreach ( $row as $cell ) {
+		$row_contents .= $this->element( 'td', $cell );
+	}
+	$table_contents .= $this->element( 'tr', $row_contents );
+}
+
+$table_contents .= '</tbody>';
+
+echo $this->element( 'table', $table_contents, array_merge( array( 'class' => 'widefat' ), $table_attributes ) );
